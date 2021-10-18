311dcd7d679301c40fb85874299608b06b34b829
Huy Duong (mint) <37185724+bicoi99@users.noreply.github.com>
2021-10-18 22:31:41 +0100

    Edit auth routes to use .then/.catch

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,92 +3,110 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-// Remove route later, disable registering
-router.post("/register", async (req, res) => {
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+router.post("/register", (req, res) => {
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return res.status(500).send({
+        message: "Server error",
+      });
+    }
+    bcrypt.hash(req.body.password, salt, (err, hashedPassword) => {
+      if (err) {
+        return res.status(500).send({
+          message: "Server error",
+        });
+      }
 
-  const user = new User({
-    username: req.body.username,
-    password: hashedPassword,
-  });
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      });
 
-  const result = await user.save();
-  const { password, ...data } = await result.toJSON();
-
-  res.send(data);
-});
-
-router.post("/login", async (req, res) => {
-  const user = await User.findOne({ username: req.body.username });
-
-  // If no user found
-  if (!user) {
-    return res.status(404).send({
-      username: "Incorrect username",
-      password: "",
+      user
+        .save()
+        .then((result) => {
+          return res.status(201).send(result);
+        })
+        .catch((err) => {
+          console.log(err);
+          return res.status(500).send({
+            message: "Server error",
+          });
+        });
     });
-  }
-
-  // Compare user password with hashed password in database
-  if (!(await bcrypt.compare(req.body.password, user.password))) {
-    return res.status(400).send({
-      username: "",
-      password: "Incorrect password",
-    });
-  }
-
-  // Generate JWT token with user id
-  const token = jwt.sign(
-    {
-      _id: user._id,
-    },
-    process.env.JWT_SECRET
-  );
-
-  // Store token in cookie
-  res.cookie("jwt", token, {
-    httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000, // 1 day
-  });
-
-  res.send({
-    message: "Success",
   });
 });
 
-router.get("/user", async (req, res) => {
-  // Get token from cookies
-  const token = req.cookies["jwt"];
+router.post("/login", (req, res) => {
+  User.findOne({ username: req.body.username })
+    .then((user) => {
+      // If username incorrect, no result from database
+      if (!user) {
+        return res.status(404).send({
+          username: "Incorrect username",
+          password: "",
+        });
+      }
 
-  // If no cookie, user is not authenticated
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        // If result is false, incorrect password
+        if (!result) {
+          return res.status(400).send({
+            username: "",
+            password: "Incorrect password",
+          });
+        }
+
+        // Generate JWT with user id
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+        // Store token to cookie
+        res.cookie("jwt", token, {
+          httpOnly: true,
+          maxAge: 24 * 60 * 60 * 1000, // 1 day
+        });
+
+        res.send({ message: "Success" });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).send({
+        message: "Server error",
+      });
+    });
+});
+
+router.get("/user", (req, res) => {
+  // Retrieve JWT from cookie
+  const token = req.cookies.jwt;
+
+  // If no cookie, user not authenticated
   if (!token) {
     return res.status(401).send({
       message: "Unauthenticated",
     });
   }
 
-  // Decode token
-  jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-    // If token is invalid
+  // Decode
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    // If token invalid
     if (err) {
       return res.status(401).send({
         message: "Unauthenticated",
       });
     }
 
-    // Find user by id in database
-    const user = await User.findById(decoded._id);
-    const { username } = await user.toJSON(); // do we need to return id as well?
-
-    res.send(username);
+    User.findById(decoded._id).then(({ username }) => {
+      return res.send(username);
+    });
   });
 });
 
 router.post("/logout", (req, res) => {
-  res.cookie("jwt", "", { maxAge: 0 }); // create new jwt cookie expires immediately
+  res.cookie("jwt", "", { maxAge: 0 });
 
-  res.send({ message: "Success" });
+  return res.send({ message: "Success" });
 });
 
 module.exports = router;
