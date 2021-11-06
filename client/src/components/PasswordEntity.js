ac5c53e09a0c74cdee795e312bf7972126875d5b
Huy Duong (mint) <37185724+bicoi99@users.noreply.github.com>
2021-11-06 23:46:37 +0000

    Change password entity design and add copy functionality

--- client/src/components/PasswordEntity.js
+++ client/src/components/PasswordEntity.js
@@ -5,6 +5,7 @@ import "../css/PasswordEntity.css";
 const PasswordEntity = ({ apiUrl, _id, appName, username, setShowEdit, setShowDelete, setCurrentPassword }) => {
   const [showPassword, setShowPassword] = useState(true);
   const [password, setPassword] = useState("password");
+  const [copied, setCopied] = useState(false);
 
   const hideShowPassword = () => {
     if (showPassword) {
@@ -23,35 +24,50 @@ const PasswordEntity = ({ apiUrl, _id, appName, username, setShowEdit, setShowDe
     setShowPassword(!showPassword);
   };
 
+  const handleCopied = () => {
+    navigator.clipboard.writeText(password);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
   return (
     <div className="password-entity">
       <div className="info">
         <h1>{appName}</h1>
         <h3>{username}</h3>
-        <div className="password">
-          <h3>{showPassword ? String.fromCharCode(8226).repeat(20) : password}</h3>
+        <h3>{showPassword ? String.fromCharCode(8226).repeat(20) : password}</h3>
+      </div>
+      <div className="password-btns">
+        <div className="edit-delete">
+          <i
+            className="fas fa-lg fa-edit btn"
+            onClick={() => {
+              setShowEdit(true);
+              setShowPassword(true);
+              setCurrentPassword({ _id, appName, username });
+            }}
+          ></i>
+          <i
+            className="fas fa-lg fa-trash btn"
+            onClick={() => {
+              setShowDelete(true);
+              setShowPassword(true);
+              setCurrentPassword({ _id, appName, username });
+            }}
+          ></i>
+        </div>
+        <div className="show-copy">
           <div className="eye" onClick={hideShowPassword}>
             {showPassword ? <i className="fas fa-lg fa-eye btn"></i> : <i className="fas fa-lg fa-eye-slash btn"></i>}
           </div>
+          {!showPassword && (
+            <div className={`copy-password${copied && " active"}`} onClick={handleCopied}>
+              <i className="fas fa-copy fa-lg btn"></i>
+            </div>
+          )}
         </div>
-      </div>
-      <div className="password-buttons">
-        <i
-          className="fas fa-lg fa-edit btn"
-          onClick={() => {
-            setShowEdit(true);
-            setShowPassword(true);
-            setCurrentPassword({ _id, appName, username });
-          }}
-        ></i>
-        <i
-          className="fas fa-lg fa-trash btn"
-          onClick={() => {
-            setShowDelete(true);
-            setShowPassword(true);
-            setCurrentPassword({ _id, appName, username });
-          }}
-        ></i>
       </div>
     </div>
   );
